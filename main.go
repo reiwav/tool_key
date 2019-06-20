1d04b745c6fca28b1d49b8e8c4386ce7175df9d4
longnhong <longnhong23@gmail.com>
2019-06-20 13:46:20 +0700

    all

---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"reiwav/tool_key/config"
+
+	"github.com/golang/glog"
+)
+
+func main() {
+	var conf = config.Read()
+	switch conf.Type {
+	case "READ":
+		fallthrough
+	case "USE":
+		var ls = config.Decode()
+		ls.Println()
+	case "NEW":
+		var err = conf.GenerateLicense()
+		if err != nil {
+			fmt.Println("ERROR: ", err)
+			glog.Errorf("ERROR: ", err)
+		}
+		fmt.Println("==== GEN OK =====")
+		conf.Println()
+	}
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+}
